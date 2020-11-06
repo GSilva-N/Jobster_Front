53ac71ffb2a786540f73cb81ba7fbf0c9d747961
unknown <cirellia5@gmail.com>
2020-11-06 12:10:47 -0300

    login jobster

--- jobsterWeb/src/app/app.module.ts
+++ jobsterWeb/src/app/app.module.ts
@@ -1,32 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.component';
-import { FeedComponent } from './feed/feed.component';
-import { HomeComponent } from './home/home.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FeedComponent } from './feed/feed.component';
+import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     NavbarComponent,
     FooterComponent,
-    PerfilLateralComponent,
     FeedComponent,
-    HomeComponent,
+    PerfilLateralComponent,
     LoginComponent,
     CadastroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
