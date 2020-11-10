f00b2c3177b7ddc1c6af00366228d330e0b24220
Guilherme <guilherme.paganini10@gmail.com>
2020-11-10 16:21:01 -0300

    Angular

--- jobsterWeb/src/app/app-routing.module.ts
+++ jobsterWeb/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { FeedComponent } from './feed/feed.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { MeuPerfilComponent } from './meu-perfil/meu-perfil.component';
+import { PostTemaComponent } from './post-tema/post-tema.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: 'feed', component: FeedComponent },
   { path: 'login', component: LoginComponent },
   { path: 'cadastro', component: CadastroComponent },
-  { path: 'meu-perfil', component: MeuPerfilComponent }
+  { path: 'meu-perfil', component: MeuPerfilComponent },
+  { path: 'cadastro-tema', component: PostTemaComponent }
 ];
 
 @NgModule({
